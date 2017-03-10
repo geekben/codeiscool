09c535060bdfcf652444b09b29e73e2062ce0238
geekben <bn0418@gmail.com>
2017-03-10 19:44:17 +0800

    a little optimization

--- topcoder/YetAnotherORProblem2.py
+++ topcoder/YetAnotherORProblem2.py
@@ -2,14 +2,18 @@ class YetAnotherORProblem2:
     def countSequences(self, n, r):
         ls = {0:1}
         c = 0
+        mc = 0
+        while (1<<mc) < r:
+            mc += 1
         for i in range(0, n+1):
             if i == n:
                 for k,v in ls.items():
                     c += v
                 return c%1000000009
             cs = {0:0}
-            for j in range(0, r+1):
-                for k,v in ls.items():
+            for k,v in ls.items():
+                mask = ((1<<mc)-1)^k
+                for j in range(0, mask+1):
                     if k & j == 0:
                         cs[k+j] =  cs.get(k+j, 0) + v
             ls = cs.copy()
@@ -20,7 +24,7 @@ if __name__ == "__main__":
     print YetAnotherORProblem2().countSequences(3,3)
     print YetAnotherORProblem2().countSequences(7,1023)
     print YetAnotherORProblem2().countSequences(2,1024)
-    #print YetAnotherORProblem2().countSequences(10,14336)
+    print YetAnotherORProblem2().countSequences(10,14336)
 
     '''
     import re
