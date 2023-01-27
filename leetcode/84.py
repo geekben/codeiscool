004f9f280c83c5169755b1d906d4d2b18adebda5
geekben <bn0418@gmail.com>
2023-01-27 22:16:39 +0800

    [84] pruning but still timeout
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/84.py
+++ leetcode/84.py
@@ -4,25 +4,27 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        a = heights[0]
-        h = heights[0]
+        h = 0
         e = 1
-        maxa = a
-        st = [[h, a]]
+        maxa = heights[0]
+        st = [[heights[0],heights[0]]]
         stl = 1 #st length
+        ls = len(heights)
 
-        for r in heights[1:]:
-            for i,s in enumerate(st[:e]):
+        for p,r in enumerate(heights[1:]):
+            for i,s in enumerate(st[h:e]):
                 k = s[0]
                 if r == k:
                     s[1] += k
-                    e = i + 1
+                    e = h + i + 1
                     if maxa < s[1]:
                         maxa = s[1]
+                    if maxa > (ls-p)*k + s[1]:
+                        h += 1
                     break
                 elif r > k:
                     s[1] += k
-                    if i + 1 == e:
+                    if h + i + 1 == e:
                         if e >= stl:
                             st.append([r,r])
                             stl += 1
@@ -33,11 +35,15 @@ class Solution(object):
                             maxa = r
                     if maxa < s[1]:
                         maxa = s[1]
+                    if maxa > (ls-p)*k + s[1]:
+                        h += 1
                 else:
-                    st[i] = [r, r+s[1]/k*r]
-                    if st[i][1] > maxa:
-                        maxa = st[i][1]
-                    e = i + 1
+                    st[h+i] = [r, r+s[1]/k*r]
+                    if st[h+i][1] > maxa:
+                        maxa = st[h+i][1]
+                    e = h + i + 1
+                    if maxa > (ls-p)*k + s[1]:
+                        h += 1
                     break
             #print r,st[:e],maxa
 
