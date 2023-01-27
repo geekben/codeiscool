924db272867cc8b56d71f996d254970800f8877d
geekben <bn0418@gmail.com>
2023-01-27 21:41:34 +0800

    [84] reduce data copy but still timeout
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/84.py
+++ leetcode/84.py
@@ -6,29 +6,39 @@ class Solution(object):
         """
         a = heights[0]
         h = heights[0]
+        e = 1
         maxa = a
         st = [[h, a]]
+        stl = 1 #st length
 
         for r in heights[1:]:
-            ls = len(st)
-            stt = st[:]
-            for i,s in enumerate(stt):
+            for i,s in enumerate(st[:e]):
                 k = s[0]
                 if r == k:
                     s[1] += k
+                    e = i + 1
+                    if maxa < s[1]:
+                        maxa = s[1]
+                    break
                 elif r > k:
                     s[1] += k
-                    if i+1 >= ls:
-                        st.append([r, r])
+                    if i + 1 == e:
+                        if e >= stl:
+                            st.append([r,r])
+                            stl += 1
+                        else:
+                            st[e] = [r, r]
+                        e += 1
+                        if r > maxa:
+                            maxa = r
+                    if maxa < s[1]:
+                        maxa = s[1]
                 else:
-                    for t in st[i:]:
-                        if t[1] > maxa:
-                            maxa = t[1]
-                    st = st[:i]
-                    st.append([r, r+s[1]/k*r])
+                    st[i] = [r, r+s[1]/k*r]
+                    if st[i][1] > maxa:
+                        maxa = st[i][1]
+                    e = i + 1
                     break
+            #print r,st[:e],maxa
 
-        for s in st:
-            if s[1] > maxa:
-                maxa = s[1]
         return maxa
