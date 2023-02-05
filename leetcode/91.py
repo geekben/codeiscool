79b7132dba9a92a9615b75eee6fe498c450b31ae
geekben <bn0418@gmail.com>
2023-02-05 20:29:47 +0800

    [91] pass beat 90% in time with memorized searching
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/91.py
+++ leetcode/91.py
@@ -1,17 +1,24 @@
 class Solution(object):
+    d = {}
     def numDecodings(self, s):
         """
         :type s: str
         :rtype: int
         """
+        d = self.d
+        if s in d:
+            return d[s]
         l = len(s)
         if l == 0:
+            d[s] = 1
             return 1
         if l == 1:
             n = int(s)
             if n > 0 and n < 10:
+                d[s] = 1
                 return 1
             else:
+                d[s] = 0
                 return 0
         else:
             n = int(s[-1])
@@ -22,4 +29,5 @@ class Solution(object):
                 n = int(s[-2:])
                 if n > 9 and n < 27:
                     b = self.numDecodings(s[:-2])
+            d[s] = a + b
             return a + b
