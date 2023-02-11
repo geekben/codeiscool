77cc28afc30a147ab831020f3923fdf0f3f2db81
geekben <bn0418@gmail.com>
2023-02-11 22:08:16 +0800

    [97] memorized search pass, only 7%
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/97.py
+++ leetcode/97.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    d = {}
     def isInterleave(self, s1, s2, s3):
         """
         :type s1: str
@@ -6,6 +7,9 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
+        d = self.d
+        if (s1,s2,s3) in d:
+            return d[(s1,s2,s3)]
         l1 = len(s1)
         l2 = len(s2)
         hs = s1
@@ -16,12 +20,16 @@ class Solution(object):
 
         if l1 == 0 or l2 == 0:
             if ts == s3:
+                d[(s1,s2,s3)] = True
                 return True
             else:
+                d[(s1,s2,s3)] = False
                 return False
         h = hs[0]
         for i,c in enumerate(s3):
             if c == h and ts[:i] == s3[:i] and \
                 self.isInterleave(hs[1:], ts[i:], s3[i+1:]):
+                d[(s1,s2,s3)] = True
                 return True
+        d[(s1,s2,s3)] = False
         return False
