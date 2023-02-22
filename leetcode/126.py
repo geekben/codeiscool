972d62bf84f25aff02068fb1e8c80f74f89c347b
geekben <bn0418@gmail.com>
2023-02-22 23:35:12 +0800

    [126] BFS pass 44% 100%
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/126.py
+++ leetcode/126.py
@@ -9,12 +9,13 @@ class Solution(object):
         l = len(beginWord)
         ws = set(wordList)
         q = [[beginWord]]
-        st = {}
         res = []
+        wq = []
         while q:
             wl = q.pop(0)
             if res and len(wl) >= len(res[0]):
                 break
+            wq.append(wl)
             w = wl[-1]
             for i in xrange(l):
                 for n in xrange(97, 123):
@@ -25,5 +26,25 @@ class Solution(object):
                             res.append(wl + [nw])
                         if res == []:
                             q.append(wl + [nw])
- 
+                            ws.remove(nw)
+
+        if res:
+            rl = len(res[0])
+            st = [[] for _ in xrange(rl)]
+            for i in xrange(rl):
+                for r in res:
+                    st[i].append(r[i])
+
+            for wl in wq[1:]:
+                ll = len(wl)
+                w = wl[-1]
+                for i in xrange(l):
+                    for n in xrange(97, 123):
+                        c = chr(n)
+                        nw = w[:i]+c+w[i+1:]
+                        if nw in st[ll]:
+                            ri = st[ll].index(nw)
+                            tr = wl + res[ri][ll:]
+                            if tr not in res:
+                                res.append(tr)
         return res
