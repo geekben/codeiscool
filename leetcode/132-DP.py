f94787f3a4f501087eedfe9f4052256b68a60e62
geekben <bn0418@gmail.com>
2023-02-28 16:22:58 +0800

    [132] DP pass, should dive deeper
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/132-DP.py
+++ leetcode/132-DP.py
@@ -10,35 +10,30 @@
 "abbbbaabcdefghijklmnopqrstuvwxyzzzzzzyyyyxxxxxabcd"
 '''
 class Solution(object):
-    st = {}
-    def isPalindrome(self, s):
-        if s in self.st:
-            return self.st[s]
-        ls = len(s)
-        if s[0] == s[-1]:
-            if ls <= 2:
-                self.st[s] = True
-                return True
-            elif self.isPalindrome(s[1:-1]):
-                self.st[s] = True
-                return True
-            else:
-                self.st[s] = False
-                return False
-        else:
-            self.st[s] = False
-            return False
-
     def minCut(self, s):
         """
         :type s: str
         :rtype: int
         """
-        if self.isPalindrome(s):
-            return 0
         ls = len(s)
-        ret = 2001
+        d = [[False for _ in xrange(ls)] for _ in xrange(ls)]
+
+        for j in xrange(ls):
+            for i in xrange(j+1):
+                if i == j:
+                    d[i][j] = True
+                elif j - i == 1 and s[i] == s[j]:
+                    d[i][j] = True
+                elif s[i] == s[j] and d[i+1][j-1]:
+                    d[i][j] = True
+
+        ret = [2001 for _ in xrange(ls)]
         for i in xrange(ls):
-            if self.isPalindrome(s[i:]):
-                ret = min(ret, self.minCut(s[:i]))
-        return ret + 1
+            if d[0][i]:
+                ret[i] = 0
+            else:
+                for j in xrange(i):
+                    if d[j+1][i]:
+                        ret[i] = min(ret[i], ret[j]+1)
+        #print ret
+        return ret[ls-1] # the string end at ls-1 index
