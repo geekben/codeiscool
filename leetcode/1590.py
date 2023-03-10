e0c02fe2f5762e1f017ed42465de2ffb77054e75
geekben <bn0418@gmail.com>
2023-03-10 11:18:38 +0800

    [1590] add some comments of solution
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/1590.py
+++ leetcode/1590.py
@@ -1,3 +1,28 @@
+'''
+F[i] = sum(nums[:i+1]
+theorem1:
+if F[j] mod p == F[i] mod p (j < i);then
+    (F[i] - F[j]) mod p == 0
+fi
+
+theorem2:
+if F[i] mod p == x;then
+    (F[i] - x) mod p == 0
+fi
+
+we should find a subarray (j,i] which meets:
+(F[i] - F[j]) mod p == x (x = (sum(nums) mod p))
+so that the sum of remaining array mod p equals 0
+
+according to theorem2:
+    (F[i] - F[j]) mod p == x
+=>  (F[i] - F[j] - x) mod p == 0
+
+according to theorem1:
+    (F[i] - F[j] - x) mod p == 0
+we need found (i,j) that meets:
+    (F[i] - x) mod p == F[j] mod p
+'''
 class Solution(object):
     def minSubarray(self, nums, p):
         """
