57f27ebdeb25ae39a6c66f2a8d1b8f19e407fa57
geekben <bn0418@gmail.com>
2023-03-12 18:50:12 +0800

    [1617] copy solution 100% 100%
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/1617.py
+++ leetcode/1617.py
@@ -6,25 +6,46 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        subs = [[] for _ in xrange(n-1)]
+        dist = [[n+1]*n for _ in xrange(n)]
+        for i in xrange(n):
+            dist[i][i] = 0
+        dp = [0]*(1<<n)
 
-        for i in xrange(1, n+1):
-            for e in edges[:]:
-                se = sorted(e)
-                if se[0] != i:
+        for x,y in edges:
+            dp[(1<<(x-1)) + (1<<(y-1))] = 1
+            dist[x-1][y-1] = 1
+            dist[y-1][x-1] = 1
+
+        for k in xrange(n):
+            for i in xrange(n):
+                for j in xrange(n):
+                    temp = dist[i][k] + dist[k][j]
+                    if temp < dist[i][j]:
+                        dist[i][j] = temp
+
+        for i in xrange(1<<n):
+            if dp[i] == 0:
+                continue
+            for j in xrange(n):
+                bj = 1 << j
+                if i & bj != 0 or dp[i | bj] != 0:
                     continue
-                edges.remove(e)
-                for j in xrange(n-2,-1,-1):
-                    for ne in subs[j]:
-                        if se[0] == ne[0]:
-                            subs[j+1].append(se[1] + ne[:])
-                        elif se[0] == ne[-1]:
-                            subs[j+1].append(ne[:] + se[1])
-                        elif se[0] < ne[1] and se[0] > ne[0]:
-                            subs[j].append([ne[0], ne[1]])
-                subs[0].append(se)
 
-        ret = []
-        for sub in subs:
-            ret.append(len(sub))
+                for k in xrange(n):
+                    bk = 1 << k
+                    if i & bk != 0 and dist[k][j] == 1:
+                        dp[i|bj] = dp[i]
+                        break
+                if dp[i|bj] == 0:
+                    continue
+                for k in xrange(n):
+                    bk = 1 << k
+                    if i & bk != 0:
+                        dp[i|bj] = max(dp[i|bj], dist[k][j])
+
+        ret = [0]*(n-1)
+        for i in range(1<<n):
+            if dp[i] != 0:
+                ret[dp[i]-1] += 1
+
         return ret
