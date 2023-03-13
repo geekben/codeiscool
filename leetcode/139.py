6dc36f3acb111b43ea08b97af9decf33db1c8a33
geekben <bn0418@gmail.com>
2023-03-14 00:16:39 +0800

    [139] better solution, 27 62
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/139.py
+++ leetcode/139.py
@@ -6,16 +6,12 @@ class Solution(object):
         :rtype: bool
         """
         ls = len(s)
-        dp = [[False] * ls for _ in xrange(ls)]
+        dp = [False] * (ls + 1)
+        dp[0] = True
 
         for i in xrange(ls):
-            for j in xrange(i+1,ls+1):
-                if s[i:j] in wordDict:
-                    dp[i][j-1] = True
+            for j in xrange(0,i+1):
+                if dp[j] and s[j:i+1] in wordDict:
+                    dp[i+1] = True
         
-        for j in xrange(ls):
-            if not dp[0][j]:
-                for k in xrange(1,j+1):
-                    if dp[0][k-1] and dp[k][j]:
-                        dp[0][j] = True
-        return dp[0][ls-1]
+        return dp[ls]
