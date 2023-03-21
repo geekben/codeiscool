e19dd3bfde2722af204833fe0ba6372f2f5bbe72
geekben <bn0418@gmail.com>
2023-03-22 01:23:42 +0800

    [1626] dp, 83 50
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/1626.py
+++ leetcode/1626.py
@@ -6,30 +6,15 @@ class Solution(object):
         :rtype: int
         """
         ll = len(scores)
-        #conf = [[True] * ll for _ in xrange(ll)]
-        conf = []
+        tp = [[scores[i],ages[i]] for i in xrange(ll)]
+        dp = [0] * ll
+        sp = sorted(tp, key=lambda p:(p[0],p[1]))
+        ret = 0
 
         for i in xrange(ll):
-            for j in xrange(i+1, ll):
-                if (scores[i] > scores[j] and ages[i] < ages[j]) or \
-                    (scores[i] < scores[j] and ages[i] > ages[j]):
-                    #conf[i][j] = False
-                    conf.append((1 << i) | (1 << j))
-        # print conf
-
-        m = 0
-        for i in xrange(1,1<<ll):
-            if i in conf:
-                continue
-            isConf = False
-            for c in conf:
-                if (i & c) == c:
-                    isConf = True
-                    break
-            if not isConf:
-                s = 0
-                for j in xrange(ll):
-                    if ((1 << j) & i) > 0:
-                        s += scores[j]
-                m = max(m, s)
-        return m
+            for j in xrange(i):
+                if sp[i][1] >= sp[j][1]:
+                    dp[i] = max(dp[i], dp[j])
+            dp[i] += sp[i][0]
+            ret = max(ret, dp[i])
+        return ret
