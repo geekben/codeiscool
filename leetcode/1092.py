6c74c8f9b1e53f0d7b99df016a30ef76cd322952
geekben <bn0418@gmail.com>
2023-03-28 23:41:17 +0800

    [1092] dp pass 0 0
    Signed-off-by: geekben <bn0418@gmail.com>

--- leetcode/1092.py
+++ leetcode/1092.py
@@ -1,46 +1,63 @@
+'''
+"abac"
+"cab"
+"aaaaaaaa"
+"aaaaaaaa"
+"abc"
+"xyz"
+"a"
+"a"
+"ab"
+"a"
+"ababab"
+"baa"
+"atbasabad"
+"tsdb"
+"bcaaacbbbcbdcaddadcacbdddcdcccdadadcbabaccbccdcdcbcaccacbbdcbabb"
+"dddbbdcbccaccbababaacbcbacdddcdabadcacddbacadabdabcdbaaabaccbdaa"
+"atdznrqfwlfbcqkezrltzyeqvqemikzgghxkzenhtapwrmrovwtpzzsyiwongllqmvptwammerobtgmkpowndejvbuwbporfyroknrjoekdgqqlgzxiisweeegxajqlradgcciavbpgqjzwtdetmtallzyukdztoxysggrqkliixnagwzmassthjecvfzmyonglocmvjnxkcwqqvgrzpsswnigjthtkuawirecfuzrbifgwolpnhcapzxwmfhvpfmqapdxgmddsdlhteugqoyepbztspgojbrmpjmwmhnldunskpvwprzrudbmtwdvgyghgprqcdgqjjbyfsujnnssfqvjhnvcotynidziswpzhkdszbblustoxwtlhkowpatbypvkmajumsxqqunlxxvfezayrolwezfzfyzmmneepwshpemynwzyunsxgjflnqmfghsvwpknqhclhrlmnrljwabwpxomwhuhffpfinhnairblcayygghzqmotwrywqayvvgohmujneqlzurxcpnwdipldofyvfdurbsoxdurlofkqnrjomszjimrxbqzyazakkizojwkuzcacnbdifesoiesmkbyffcxhqgqyhwyubtsrqarqagogrnaxuzyggknksrfdrmnoxrctntngdxxechxrsbyhtlbmzgmcqopyixdomhnmvnsafphpkdgndcscbwyhueytaeodlhlzczmpqqmnilliydwtxtpedbncvsqauopbvygqdtcwehffagxmyoalogetacehnbfxlqhklvxfzmrjqofaesvuzfczeuqegwpcmahhpzodsmpvrvkzxxtsdsxwixiraphjlqawxinlwfspdlscdswtgjpoiixbvmpzilxrnpdvigpccnngxmlzoentslzyjjpkxemyiemoluhqifyonbnizcjrlmuylezdkkztcphlmwhnkdguhelqzjgvjtrzofmtpuhifoqnokonhqtzxmimp"
+"xjtuwbmvsdeogmnzorndhmjoqnqjnhmfueifqwleggctttilmfokpgotfykyzdhfafiervrsyuiseumzmymtvsdsowmovagekhevyqhifwevpepgmyhnagjtsciaecswebcuvxoavfgejqrxuvnhvkmolclecqsnsrjmxyokbkesaugbydfsupuqanetgunlqmundxvduqmzidatemaqmzzzfjpgmhyoktbdgpgbmjkhmfjtsxjqbfspedhzrxavhngtnuykpapwluameeqlutkyzyeffmqdsjyklmrxtioawcrvmsthbebdqqrpphncthosljfaeidboyekxezqtzlizqcvvxehrcskstshupglzgmbretpyehtavxegmbtznhpbczdjlzibnouxlxkeiedzoohoxhnhzqqaxdwetyudhyqvdhrggrszqeqkqqnunxqyyagyoptfkolieayokryidtctemtesuhbzczzvhlbbhnufjjocporuzuevofbuevuxhgexmckifntngaohfwqdakyobcooubdvypxjjxeugzdmapyamuwqtnqspsznyszhwqdqjxsmhdlkwkvlkdbjngvdmhvbllqqlcemkqxxdlldcfthjdqkyjrrjqqqpnmmelrwhtyugieuppqqtwychtpjmloxsckhzyitomjzypisxzztdwxhddvtvpleqdwamfnhhkszsfgfcdvakyqmmusdvihobdktesudmgmuaoovskvcapucntotdqxkrovzrtrrfvoczkfexwxujizcfiqflpbuuoyfuoovypstrtrxjuuecpjimbutnvqtiqvesaxrvzyxcwslttrgknbdcvvtkfqfzwudspeposxrfkkeqmdvlpazzjnywxjyaquirqpinaennweuobqvxnomuejansapnsrqivcateqngychblywxtdwntancarldwnloqyywrxrganyehkglbdeyshpodpmdchbcc"
+'''
 class Solution(object):
-    dp = {}
-    str1 = ""
-    str2 = ""
-
-    def superseq(self, i, j):
-        if (i,j) in self.dp:
-            return self.dp[(i,j)]
-        str1 = self.str1[i:]
-        str2 = self.str2[j:]
-        l1 = len(str1)
-        l2 = len(str2)
-        ret = ""
-        if l1 == 0:
-            self.dp[(i,j)] = str2
-            return str2
-        if l2 == 0:
-            self.dp[(i,j)] = str1
-            return str1
-
-        if str1[0] == str2[0]:
-            ret = str1[0] + \
-                self.superseq(i+1, j+1)
-        else:
-            t1 = str1[0] + \
-                self.superseq(i+1, j)
-            t2 = str2[0] + \
-                self.superseq(i, j+1)
-            if len(t1) <= len(t2):
-                ret = t1
-            else:
-                ret = t2
-        self.dp[(i,j)] = ret
-        return ret
-
     def shortestCommonSupersequence(self, str1, str2):
         """
         :type str1: str
         :type str2: str
         :rtype: str
         """
-        self.dp = {}
-        self.str1 = str1
-        self.str2 = str2
-        ret = self.superseq(0,0)
+        n = len(str1)
+        m = len(str2)
+
+        dp = [[0] * (m+1) for _ in xrange(n+1)]
+        for i in xrange(n):
+            dp[i][m] = n - i
+        for i in xrange(m):
+            dp[n][i] = m - i
+        for i in xrange(n-1,-1,-1):
+            for j in xrange(m-1,-1,-1):
+                if str1[i] == str2[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+                else:
+                    dp[i][j] = min(dp[i+1][j], dp[i][j+1])+1
+
+        ret = ""
+        i = j = 0
+        while i < n and j < m:
+            if str1[i] == str2[j]:
+                ret += str1[i]
+                i += 1
+                j += 1
+            elif dp[i][j] - 1 == dp[i+1][j]:
+                ret += str1[i]
+                i += 1
+            #elif dp[i][j] - 1 == dp[i][j+1]:
+            else:
+                ret += str2[j]
+                j += 1
+            #else:
+                #print i,j,dp[i][j],dp[i+1][j],dp[i][j+1]
+        if i < n:
+            ret += str1[i:]
+        elif j < m:
+            ret += str2[j:]
         return ret
